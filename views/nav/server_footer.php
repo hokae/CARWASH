4a315944b8b00ffa2f8481502f5d0854d5fd2c96
Naruto_25 <espielreo635@gmail.com>
2024-12-05 09:48:17 +0800

    23rd commit

--- views/nav/server_footer.php
+++ views/nav/server_footer.php
@@ -40,17 +40,17 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/dist/jsvendor/jquery/jquery.min.js"></script>
+    <script src="../assets/dist/jsvendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="../assets/dist/jsvendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="../assets/dist/js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="../assets/dist/jsvendor/chart.js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
     <script src="../assets/dist/js/demo/chart-area-demo.js"></script>
