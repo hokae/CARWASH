104cec7c1b48151886411035d0b3dec07b90dac0
Naruto_25 <espielreo635@gmail.com>
2024-12-05 13:15:09 +0800

    59th commit

--- views/nav/header.php
+++ views/nav/header.php
@@ -15,7 +15,9 @@
         <!-- Responsive navbar-->
         <nav class="navbar navbar-expand-lg">
             <div class="container px-5">
-                <a class="navbar-brand" href="#!">Start Bootstrap</a>
+            <a class="navbar-brand" href="#!">
+                <img src="../images/logo.png" alt="Logo" style="height: 150px;">
+            </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
