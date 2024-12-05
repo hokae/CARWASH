77668002906f5d146636611c5cc78a68f488dd28
Naruto_25 <espielreo635@gmail.com>
2024-12-05 09:14:44 +0800

    18th commit

--- views/nav/server_footer.php
+++ views/nav/server_footer.php
@@ -47,14 +47,14 @@
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="../assets/dist/js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
     <script src="vendor/chart.js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="js/demo/chart-area-demo.js"></script>
-    <script src="js/demo/chart-pie-demo.js"></script>
+    <script src="../assets/dist/js/demo/chart-area-demo.js"></script>
+    <script src="../assets/dist/js/demo/chart-pie-demo.js"></script>
 
 </body>
 
