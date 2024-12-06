e49a56e9d99f8e0d02c1e968c2e2a7d2011f53c2
James_np <jamesnathaneilpoto02@gmail.com>
2024-12-06 11:36:12 +0800

    77th commit

--- views/login.php
+++ views/login.php
@@ -42,7 +42,7 @@
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
-                                    <form action="../page/authentication.php?function=login&&sub_page=login" method="$_POST" class="user">
+                                    <form action="../views/home.php" method="$_POST" class="user">
                                         <div class="form-group">
                                             <input type="email" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
