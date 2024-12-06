7a5fbde1645253d7213b553ef2748f10eb138dd9
Naruto_25 <espielreo635@gmail.com>
2024-12-06 11:03:35 +0800

    69th commit

--- views/home.php
+++ views/home.php
@@ -44,13 +44,13 @@
 
       <a href="index.html" class="logo d-flex align-items-center">
         <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <img src="../images/logo.png" alt="">
         <h1 class="sitename">Nova</h1>
       </a>
 
       <nav id="navmenu" class="navmenu">
         <ul>
-          <li><a href="home.php" class="active">Home<br></a></li>
+          <li><a href="home.php" class="active">Home<br></a></li>   
           <li><a href="about.php">About</a></li>
           <li><a href="services.php">Services</a></li>
           <li><a href="contact.php">Contact</a></li>
