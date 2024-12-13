5c2d364d07d13d3e3c2b40f34ccdf1ce482a7490
James_np <jamesnathaneilpoto02@gmail.com>
2024-12-13 10:09:13 +0800

    118 commit

--- views/booking.php
+++ views/booking.php
@@ -27,7 +27,7 @@
             padding: 20px;
             background-color: white;
             box-shadow: 0 4px 6px rgba(0,0,0,0.1);
-            border-radius: 10px;
+            
         }
         .header {
             text-align: center;
